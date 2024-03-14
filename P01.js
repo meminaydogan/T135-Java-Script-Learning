de021aedef26ad03206f00317b9032c54819eccb
meminaydogan <meminaydogan99@gmail.com*git config --global user.email>
2024-03-14 14:04:52 +0300

    first commit

---
+++ P01.js
@@ -0,0 +1,25 @@
+// console.log kullanımı
+// system.out.println işlevini görür.
+console.log("Muhammed");
+console.log('Aydogan');
+console.log('Hello World');
+
+
+// değişken yazdırma
+let name = 'Emin Aydogan';
+console.log (name);
+
+
+let sayi1=20;
+let sayi2=30;
+
+console.log(sayi1+sayi2); // 50
+console.log('sayi1'+'sayi2') // sayi1sayi2
+// console.log(sayi1sayi2) // hata verir
+console.log(20+30);
+console.log('30'+'20');
+console.log('Sn.' + name + 'Hoşgeldiniz')
+
+
+
+console.log('Hello World \n "Hello World"')
